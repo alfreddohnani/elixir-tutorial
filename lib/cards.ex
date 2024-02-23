49c91cbc813febb97e5f247dbea2f04ef6766a71
Alfred Doh-Nani <alfreddohnani@hotmail.com>
2024-02-23 15:23:47 +0000

    🤯: error handling is insane

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,19 +1,50 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
+  def hello do
+    "hi there"
+  end
 
-  @spec create(any()) :: [<<_::24, _::_*16>>, ...]
-  @doc """
-  Hello world.
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hears", "Diamonds"]
 
-  ## Examples
+    # Not great impl
 
-      iex> Cards.hello()
-      :world
+    # cards =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+    # List.flatten(cards)
 
-  """
-  def create(deck) do
-    ["Ace", "Two", "Three"]
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
   end
 end
