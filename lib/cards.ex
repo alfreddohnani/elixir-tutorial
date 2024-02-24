a56b5a19ac064e190ef6418fb24d55ec2b1e5eec
Alfred Doh-Nani <alfreddohnani@hotmail.com>
2024-02-24 15:45:53 +0000

    documenting your code

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,14 @@
 defmodule Cards do
+  @moduledoc """
+  Provides methods for creating and handling a deck of cards
+  """
   def hello do
     "hi there"
   end
 
+  @doc """
+  Returns a list of string s representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hears", "Diamonds"]
@@ -30,6 +36,16 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Divides a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Aces of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -44,5 +60,13 @@ defmodule Cards do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "That file does not exist"
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+
+    # Elixir passes `deck` as the first argument to deal
   end
 end
