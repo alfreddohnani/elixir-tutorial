d807864e048a0e7738563447f53d2847939a4348
Alfred Doh-Nani <alfreddohnani@hotmail.com>
2024-02-24 16:11:05 +0000

    add doctest and case test

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -3,6 +3,18 @@ defmodule CardsTest do
   doctest Cards
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert Cards.hello() == "hi there"
+  end
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    # assert deck != Cards.shuffle(deck)
+    refute deck == Cards.shuffle(deck)
+    # refute is the opposite of assert
   end
 end
